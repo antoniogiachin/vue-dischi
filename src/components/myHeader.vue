d5340db661c6683907ab78d7f26a4366a0efebfb
antoniogiachin <giachinantonio@outlook.com>
2022-02-23 15:00:39 +0100

    inserito component myHeader e myMain

---
+++ src/components/myHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container-fluid">
+    
+    <div class="navbar my_navbar">
+        
+        <a class="navbar-brand my_style-brand" href="#">
+        <img :src="img" alt="soundcloud-img" width="100" height="50">
+        </a>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'myHeader',
+
+    data(){
+        return{
+            img: require("../assets/soundcloud.png")
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/general.scss";
+
+.my_navbar{
+    background-color: #323d48;
+}
+</style>
---
+++ src/components/myMain.vue
@@ -0,0 +1,23 @@
+<template>
+    <main class="my_main">
+
+    </main>
+</template>
+
+<script>
+export default {
+    name:'myMain'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/general.scss";
+
+.my_main{
+    height: calc(100vh - 50px);
+    width: 100%;
+    background-color: #23313d;
+}
+
+
+</style>
