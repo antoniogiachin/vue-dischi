82beaa5f42944569cf83be61bc068b30647c036c
antoniogiachin <giachinantonio@outlook.com>
2022-02-24 15:21:45 +0100

    inserita select statica

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -4,11 +4,17 @@
 
         <div class="container-fluid p-0">
             
-            <div class=" my_navbar">
+            <div class=" my_navbar d-flex align-items-center justify-content-between pe-2 ">
                 
                 <a class="navbar-brand my_style-brand" href="#">
                     <img :src="img" alt="soundcloud-img" width="100" height="50">
                 </a>
+
+                <select name="genre-select" id="genre" class="float-end d-inline-block">
+                    <!-- da dinamicizzare -->
+                    <option value="">Scegli il genere musicale</option>
+                    <option value="genere1">Genere 1</option>
+                </select>
 
             </div>
 
